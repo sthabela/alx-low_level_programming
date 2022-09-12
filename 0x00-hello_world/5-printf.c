3ffc0289f56dcd165ba07487cafac996f838a851
sthabela <thabangseitibaleng@gmail.com>
2022-09-12 10:21:45 -0700

    task 4

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+
+#include <time.h>
+
+#include <stdio.h>
+
+/**
+ *
+ *  * main - prints all single digit numbers of base 10
+ *
+ *   * Description: using the main function
+ *
+ *    * Return: Always 0
+ *
+ *     */
+
+int main(void)
+
+{
+
+	char n;
+
+	for (n = '0'; n <= '9'; n++)
+
+	{
+
+		 putchar(n);
+
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}

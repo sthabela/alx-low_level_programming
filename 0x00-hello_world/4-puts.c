5f72cbfd9dd7ab13f969d7aed025bc56cec3a796
sthabela <thabangseitibaleng@gmail.com>
2022-09-12 10:14:49 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,14 +3,15 @@
 #include <string.h>
 
 /**
- * main - Entry point
- *
- *  *
- *
- *   * Return: Always 0 (Success)
- *
- *
- */    
+  * main - Entry point
+  *
+  *  * Description: Print the last digits
+  *
+  *   * this program prints greater than 5, less than zero or less than 6 and not 0
+  *
+  *    * Return: Always 0
+  *
+  */
 int main(void)
 
 {

8a88dee8b09ad7815a2ebc6b3059441c391c416a
sthabela <thabangseitibaleng@gmail.com>
2022-09-12 08:22:04 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,17 @@
- #include <stdio.h>
-
-
+#include <stdio.h>
 
 /**
- *
- * * main - print the string in the put function
- *
- * *
- *
- * * Description: using the main function
- *
- * * this program prints "Programming is like building a multilingual puzzle
- *
- * * Return: 0
- *
- * */
+* main - print the string in the put function
+*
+* *
+*
+* * Description: using the main function
+*
+* * this program prints "Programming is like building a multilingual puzzle
+*
+* * Return: 0
+*
+* */
 
 int main(void)
 

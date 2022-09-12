b3afbff759854b773fa85ea871150d462fd83ac8
sthabela <thabangseitibaleng@gmail.com>
2022-09-12 14:09:34 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+#include<stdlib.h>
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     *
+ *
+  */
+
+int main(void)
+
+{
+
+		char w;
+
+
+
+			for (w = 'a'; w <= 'z'; w++)
+
+						if (w != 'e' && w != 'q')
+
+										putchar(w);
+
+				putchar('\n');
+
+					return (0);
+
+}

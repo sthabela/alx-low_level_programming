fbb746cbd78dcfbb65d4736f3681d098a3c6f65d
sthabela <thabangseitibaleng@gmail.com>
2022-09-13 12:26:14 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,21 +3,41 @@
 
 
 /**
- *main - Entry point
- *
- * author: tuneday
- * Return: 0 if succesful
+
+ * main - print putchar
+
+ * Return: always 0
+
  */
-int main(void) 
+
+
+
+int main(void)
+
 {
+
 	_putchar('_');
+
 	_putchar('p');
+
 	_putchar('u');
+
 	_putchar('t');
+
 	_putchar('c');
+
 	_putchar('h');
+
 	_putchar('a');
-	_putchar('r')i;
+
+	_putchar('r');
+
 	_putchar('\n');
+
+
+
 	return (0);
+
+
+
 }

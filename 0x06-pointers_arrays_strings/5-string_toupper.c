2c9c30ede02614b88cc20b6062ccba5f4f991478
sthabela <thabangseitibaleng@gmail.com>
2022-09-22 09:02:09 -0700

    task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -13,9 +13,11 @@ char *string_toupper(char *str)
 
 	while (str[i] != '\0')
 	{
+
 		if (str[i] >= 'a' && str[i] <= 'z')
 			str[i] -= 32;
 		i++;
+	
 	}
-        return (str);
-}	
+	return (str);
+}
